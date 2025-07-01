2655866e32e4fb76e44179640916186b9e9152a5
Ari <genesisarianacordero2005@gmail.com>
2025-07-01 09:42:12 -0400

    Tarea #747: Implementar endpoint GET /api/roles

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
@@ -7,17 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+import org.springframework.web.servlet.function.RouterFunctions;
+import upb.edu.AuthMicroservice.controllers.RoleController;
 
 @Configuration
 public class Routes {
 
-    @Autowired
-    private RoleRoutes roleRoutes;
+    //@Autowired
+    //private RoleRoutes roleRoutes;
 
     @Bean
-    public RouterFunction<ServerResponse> routerFunction() {
-        return route()
-                .path("api/roles", roleRoutes::roleRouter)
+    public RouterFunction<ServerResponse> routerFunction(RoleController roleController) {
+        return RouterFunctions.route()
+                .path("/api", builder -> builder.add(RoleRoutes.roleRoutes(roleController)))
                 .build();
     }
 
