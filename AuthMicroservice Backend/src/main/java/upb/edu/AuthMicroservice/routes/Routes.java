a85749b566c47c85fed4fd185b9b6d63e40106ca
Fabian <faboazenas@gmail.com>
2025-07-04 21:58:44 -0400

    [#763] dejo lógica en UserController

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
@@ -2,20 +2,13 @@ package upb.edu.AuthMicroservice.routes;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
-
 import org.springframework.web.servlet.function.RouterFunction;
-import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 import upb.edu.AuthMicroservice.controllers.UserController;
 import upb.edu.AuthMicroservice.controllers.SessionController;
-import upb.edu.AuthMicroservice.models.LoginRequest;
-import upb.edu.AuthMicroservice.models.Response;
-import upb.edu.AuthMicroservice.models.User;
-
-import org.springframework.http.ResponseEntity;
 
 @Configuration
 public class Routes {
@@ -31,29 +24,15 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> userRoutes() {
         return route()
-                .POST("/register-user", request -> {
-                    User user = request.body(User.class);
-                    User created = userController.registerUser(user);
-                    return ServerResponse
-                            .status(201)
-                            .body(new Response("201", "OK"));
-                })
-                .POST("/login", request -> {
-                    LoginRequest dto = request.body(LoginRequest.class);
-                    ResponseEntity<Object> serviceResp = userController.login(dto);
-                    return ServerResponse
-                            .status(serviceResp.getStatusCodeValue())
-                            .body(serviceResp.getBody());
-                })
+                .POST("/register-user", userController::registerUser)
+                .POST("/login",        userController::login)
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> sessionRoutes() {
         return route()
-                .POST("/generate-session", request -> {
-                    return sessionController.generateSession(request);
-                })
+                .POST("/generate-session", sessionController::generateSession)
                 .build();
     }
 }
