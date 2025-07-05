ac7e1858186df0c057d0d43da9525fd7b4ef588e
Fabian <faboazenas@gmail.com>
2025-07-04 21:20:09 -0400

    [#763] Corrige UserController

--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/controllers/UserController.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/controllers/UserController.java
@@ -3,8 +3,6 @@ package upb.edu.AuthMicroservice.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,14 +23,12 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/register-user")
-    public User registerUser(@RequestBody User user) {
+    public User registerUser(User user) {
         log.info("Registering new user: {}", user.getEmail());
         return userService.createUser(user);
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<Object> login(@RequestBody LoginRequest loginRequest) {
+    public ResponseEntity<Object> login(LoginRequest loginRequest) {
         log.info("Login attempt for: {}", loginRequest.getEmail());
         return userService.login(loginRequest.getEmail(), loginRequest.getPassword());
     }
--- AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
+++ AuthMicroservice Backend/src/main/java/upb/edu/AuthMicroservice/routes/Routes.java
@@ -4,12 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import static org.springframework.web.servlet.function.RouterFunctions.route;
 
 import upb.edu.AuthMicroservice.controllers.UserController;
 import upb.edu.AuthMicroservice.controllers.SessionController;
+import upb.edu.AuthMicroservice.models.LoginRequest;
+import upb.edu.AuthMicroservice.models.Response;
+import upb.edu.AuthMicroservice.models.User;
+
+import org.springframework.http.ResponseEntity;
 
 @Configuration
 public class Routes {
@@ -25,15 +31,29 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> userRoutes() {
         return route()
-                .POST("/register-user", userController::registerUser)
-                .POST("/login", userController::login)
+                .POST("/register-user", request -> {
+                    User user = request.body(User.class);
+                    User created = userController.registerUser(user);
+                    return ServerResponse
+                            .status(201)
+                            .body(new Response("201", "OK"));
+                })
+                .POST("/login", request -> {
+                    LoginRequest dto = request.body(LoginRequest.class);
+                    ResponseEntity<Object> serviceResp = userController.login(dto);
+                    return ServerResponse
+                            .status(serviceResp.getStatusCodeValue())
+                            .body(serviceResp.getBody());
+                })
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> sessionRoutes() {
         return route()
-                .POST("/generate-session", sessionController::generateSession)
+                .POST("/generate-session", request -> {
+                    return sessionController.generateSession(request);
+                })
                 .build();
     }
 }
